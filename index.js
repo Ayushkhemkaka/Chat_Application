17151f4b54ee7d84bee0d36d29dd3c54613036b2
ayush-kumar-k <ayush.k@maersk.com>
2023-09-27 03:28:44 +0530

    Add Tailwind Dependencies

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on URL http://localhost:${PORT}/`);
 })
 
+app.use('/',(req,res) => res.sendFile('./views/index.html', {root: __dirname}))
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./views/tailwindcss/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
